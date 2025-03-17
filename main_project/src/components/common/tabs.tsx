bf8a82a713a2858947cc63373af7d001d1ec4bff
mj_Kim <168804702+KIMDOTS@users.noreply.github.com>
2025-03-17 15:03:03 +0900

    ✨ Tabs 컴포넌트 추가
    Tabs 컴포넌트 추가

---
+++ main_project/src/components/common/tabs.tsx
@@ -0,0 +1,22 @@
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
+function MyTabs() {
+  return (
+    <Tabs defaultIndex={0} onSelect={(index) => console.log(`Selected tab: ${index}`)}>
+      <TabList>
+        <Tab>나의 감정일기</Tab>
+        <Tab>나의 감정발자취</Tab>
+      </TabList>
+
+      <TabPanel>
+        <h2>감정일기 내용</h2>
+      </TabPanel>
+      <TabPanel>
+        <h2>감정발자취 내용</h2>
+      </TabPanel>
+    </Tabs>
+  );
+}
+
+export default MyTabs;

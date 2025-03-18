74e982dcf04777944c0ed9b67f5e7436cc788bff
mj_Kim <168804702+KIMDOTS@users.noreply.github.com>
2025-03-18 22:40:57 +0900

    💄 캘린더 UI 코드 추가
    캘린더 UI 코드 추가

--- main_project/src/pages/DiaryHome.tsx
+++ main_project/src/pages/DiaryHome.tsx
@@ -3,6 +3,7 @@ import Header from "../components/common/header";
 import Footer from "../components/common/footer";
 import HomeLayout from "../components/layouts/HomeLayout";
 import DiaryLayout from "../components/layouts/DiaryLayout";
+import MyCalendar from "../components/common/Calendar";
 
 const DiaryHome = () => {
   return (
@@ -10,7 +11,7 @@ const DiaryHome = () => {
       <Header />
       <HomeLayout>
         <DiaryLayout
-          calendarContent={<div>캘린더 섹션</div>}
+          calendarContent={<MyCalendar />}
           resultContent={<div>캘린더 결과 섹션</div>}
           diaryListContent={<div>다이어리 리스트 섹션</div>}
         />

8ab87df485472207e756de4a3c92be7ae51190a4
AN JEONG EUN <52526916+chiyo-an@users.noreply.github.com>
2025-03-20 20:13:55 +0900

    💄 헤더 로고 UI 수정

--- main_project/src/components/common/header.tsx
+++ main_project/src/components/common/header.tsx
@@ -3,8 +3,8 @@ import feelodyLogo from "../../assets/logo/logo_feelody_wh.png";
 
 const Header = () => {
   return (
-    <header className="flex justify-center items-center w-full h-[300px] bg-[#A6CCF2]">
-      <div className="w-full max-w-[380px] px-4">
+    <header className="flex justify-center items-center w-full h-[250px] bg-[#A6CCF2]">
+      <div className="w-full max-w-[330px] px-4">
         <img src={feelodyLogo} alt="Feelody logo" className="w-full h-auto" />
       </div>
     </header>

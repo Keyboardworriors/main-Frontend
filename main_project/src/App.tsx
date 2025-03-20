beb8c482dc86761cfbaa3e1fad14b4705b5e7fec
mj_Kim <168804702+KIMDOTS@users.noreply.github.com>
2025-03-20 13:31:16 +0900

    💄 Tabs 컴포넌트 UI 수정
    Tabs 컴포넌트 UI 수정

--- main_project/src/App.tsx
+++ main_project/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/common/header";
-import MyTabs from "./components/common/tabs";
+import MyTabs from "./components/Tabs/tabs";
 import Footer from "./components/common/footer";
 
 function App() {

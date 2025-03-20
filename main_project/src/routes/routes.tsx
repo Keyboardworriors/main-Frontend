cbc4e41d184b29885c868635998cfa73f0e44f60
AN JEONG EUN <52526916+chiyo-an@users.noreply.github.com>
2025-03-21 01:06:36 +0900

    Fix: login.tsx 임포트 경로 수정

--- main_project/src/routes/routes.tsx
+++ main_project/src/routes/routes.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "../components/members/Login";
+import Login from "../components/members/login";
 import Introduce from "../pages/introduce";
 import NaverCallback from "../components/members/NaverCallback";
 import ProfileSetup from "../pages/ProfileSetup";

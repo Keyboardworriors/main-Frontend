96dcbe0fe48c94e79d34a68f41aff388249fba92
minhui-p <alsgml1111@gmail.com>
2025-03-20 10:50:59 +0900

    ✨ 로그인 인증 코드 받기 실패 시 로그인페이지로 전환 추가
    로그인 인증 코드 받기 실패 시 로그인페이지로 전환 추가

--- main_project/src/components/members/KakaoCallback.tsx
+++ main_project/src/components/members/KakaoCallback.tsx
@@ -10,24 +10,26 @@ const KakaoCallback = () => {
     const getKakaoToken = async () => {
       const urlParams = new URLSearchParams(window.location.search);
       const code = urlParams.get("code");
+      if (!code) {
+        alert("인증 코드를 받아오는데 실패했습니다. 다시 시도해주세요.");
+        navigate("/login");
+        return;
+      }
+      try {
+        const response = await axios.post(`${API_BASE_URL}/oauth/kakao/callback/`, {
+          code,
+        });
+        const { accessToken, user } = response.data;
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("user", JSON.stringify(user));
 
-      if (code) {
-        try {
-          const response = await axios.post(`${API_BASE_URL}/oauth/kakao/callback/`, {
-            code,
-          });
-          const { accessToken, user } = response.data;
-          localStorage.setItem("accessToken", accessToken);
-          localStorage.setItem("user", JSON.stringify(user));
-
-          if (user.isRegistered) {
-            navigate("/ProfileSetup");
-          } else {
-            navigate("/DiaryHome");
-          }
-        } catch (error) {
-          console.error("카카오 로그인 실패", error);
+        if (user.isRegistered) {
+          navigate("/ProfileSetup");
+        } else {
+          navigate("/DiaryHome");
         }
+      } catch (error) {
+        console.error("카카오 로그인 실패", error);
       }
     };
     getKakaoToken();

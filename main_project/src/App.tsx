2cd2bf7c216eda7cec4e8b6e597f6df02c5e923c
minhui-p <alsgml1111@gmail.com>
2025-03-24 17:14:53 +0900

    💄 소셜로그인 화면 스타일 적용
    소셜로그인 화면 스타일 적용

--- main_project/src/App.tsx
+++ main_project/src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
-import Header from "./components/common/header";
-import MyTabs from "./components/common/tabs";
-import Footer from "./components/common/footer";
-import Modal from "./components/common/Modal/Modal";
+// import Header from "./components/common/header";
+// import MyTabs from "./components/common/tabs";
+// import Footer from "./components/common/footer";
+import AppRoutes from "./routes/routes";
 
 function App() {
   return (
-    <>
-      <Modal />
-      <Header />
-      <MyTabs />
-      <Footer />
-    </>
+    // <>
+    //   <Header />
+    //   <MyTabs />
+    //   <Footer />
+    // </>ㄴ
+    <AppRoutes />
   );
 }
 

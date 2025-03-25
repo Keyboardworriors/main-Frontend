a607b4850ab5e9cda1077f31ae74c38bf872bc6a
minhui-p <alsgml1111@gmail.com>
2025-03-25 13:36:55 +0900

    프로필, 마이페이지 페이지 연결

--- main_project/src/App.tsx
+++ main_project/src/App.tsx
@@ -3,14 +3,16 @@ import Header from "./components/common/header";
 import MyTabs from "./components/common/tabs";
 import Footer from "./components/common/footer";
 import Modal from "./components/common/Modal/Modal";
+import AppRoutes from "./routes/routes";
 
 function App() {
   return (
     <>
       <Modal />
-      <Header />
+      {/* <Header />
       <MyTabs />
-      <Footer />
+      <Footer /> */}
+      <AppRoutes />
     </>
   );
 }
